4e76767676a52719014c1efdb891a0580f26b549
Subhan Raj <shubhanraj2002@gmail.com>
2026-08-22 13:53:36 +0530

    Add SVG favicon, use it as the README logo

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -16,6 +16,7 @@
     <meta name="description" content="{{ Str::limit($description, 200) }}">
 
     <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32.png') }}">
     <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
